26de7111505c6d8021abaac737b2a9e0061ab4ee
liara987 <987.liara@gmail.com>
2024-12-02 13:52:56 -0300

    feat: create todo components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Input from './components/Input'
+import Button from './components/Button'
+import Header from './components/Header'
+
+function App() {  
+  return (
+    <>           
+      <Header/>   
+      <Input/>       
+      <Button/>   
+    </>
+  )
+}
+
+export default App
